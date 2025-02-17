46410f376bc6156873437984e5427c90b7a7e302
shuai <lishuailing@sifou.com>
2025-02-17 15:48:00 +0800

    fix: delete nginx.conf file

--- src/components/HomePageHeader/index.tsx
+++ src/components/HomePageHeader/index.tsx
@@ -40,7 +40,7 @@ const HomeHead: FC = () => {
 
 
   useEffect(() => {
-    fetch('/github-stars/apache/answer')
+    fetch('https://img.shields.io/github/stars/apache/answer')
       .then((response) => response.text())
       .then((data) => {
         const num = data.match(/<text .*>(.*?)<\/text>/)[1];
